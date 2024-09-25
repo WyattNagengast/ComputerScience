2335b17abb66f847b4b60e72f633d2b3ec32a1b9
WyattNagengast <820635@mystma.org>
2024-09-25 15:19:51 -0500

    Sept25

--- Assignments/AnimalSurvey.py
+++ Assignments/AnimalSurvey.py
@@ -1,6 +1,7 @@
-user_input1 = input("What is your favorite animal?\n>")
+user_input1 = input("What is your favorite animal?\n>")     #questions for user to answer lines 1-5
 user_input2 = input("What sound does the animal make?\n>")
 user_input3 = input("Where do they live?\n>")
 user_input4 = input("What is a fun fact about them?\n>")
 user_input5 = input("What does it eat?\n>")
 print("Your favorite animal is the " + user_input1 + "\nIt makes this sound " + user_input2 + "\nThey live in " + user_input3 + "\nA fun fact about them is " + user_input4 + "\nThey eat " + user_input5)
+#line 6 prints all the answers
---
+++ Unit 2/dataconversion.py
@@ -0,0 +1,14 @@
+a = str(115)         #int  ->  string
+b = str(3.14)        #float -> string
+c = int("68")        #string -> int
+d = bool("True")      #string -> boolean
+e = str(True)        #boolean -> string
+f = str(False)       #boolean -> string
+g = int('10110111')  #byte -> int
+h = float("2.54")      #string -> float
+i = float(100)         #int -> float
+j = int(10.0)        #float -> int
+k = int(254)         #int -> byte
+print(a)
+print(b)
+print(c)
