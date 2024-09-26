eece3650e8057570cf3003a63c5a03c9c70ae48f
WyattNagengast <820635@mystma.org>
2024-09-26 15:22:44 -0500

    hi

---
+++ Assignments/madlib.py
@@ -0,0 +1,10 @@
+#Once on the planet of (planet name),
+#a city (creature) left its home and (verb) into a opening in the universe.
+#Then the "creature" ended up in a (plaace) in parel.
+#Thus the "creature" had to (verb) to survive, 
+#if the "creature" wants to get back to "planet name".
+1 = input("planet name")
+2 = input("creature")
+3 = input("verb")
+4 = input("place")
+5 = input("")
