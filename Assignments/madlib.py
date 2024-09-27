8800dc9dcc608a69337c5e3ca3a59fca262d760e
WyattNagengast <820635@mystma.org>
2024-09-27 15:19:17 -0500

    Sept27

--- Assignments/madlib.py
+++ Assignments/madlib.py
@@ -1,10 +1,15 @@
 #Once on the planet of (planet name),
 #a city (creature) left its home and (verb) into a opening in the universe.
-#Then the "creature" ended up in a (plaace) in parel.
+#Then the "creature" ended up in a (place) in parel.
 #Thus the "creature" had to (verb) to survive, 
 #if the "creature" wants to get back to "planet name".
-1 = input("planet name")
-2 = input("creature")
-3 = input("verb")
-4 = input("place")
-5 = input("")
+#Then the "creature" will have to find a (space phenonama).
+1 = input("planet name ")   #inputs lines 7-12   #input the planet name.
+2 = input("creature ")  #inputs the creature.
+3 = input("verb ")      #inputs verb 1.
+4 = input("place ")     #inputs a place.
+5 = input("verb ")      #inputs verb 2.
+6 = input("space phenonama ")       #inputs a space phenonama.
+print("Once on the planet of " + 1 + ",\na city " + 2 + " left its home and " + 3 + " into a openong in the universe.\nThen the " + 2 + " ended up in a "  + 4 + " in parel.")      #prints lines 1-3
+print("Thus the " + 2 + " had to " + 5 + " to survive,\nif the " + 2 + " wants to get back to " + 1 + ".\nThen the " + 2 + " will have to find a " + 6 + ".")       #prints lines 4-6
+#print lines 13-14      #prints all the lines and inputs in the correct places.
---
+++ Unit 2/functions.py
@@ -0,0 +1,18 @@
+x = 5
+
+def top_5_movies(x, y, a, b, c):      #Define the function with a name
+    print("Top 5 movies")
+    print("1. " + x)
+    print("2. " + y)
+    print("3. " + a)
+    print("4. " + b)
+    print("5. " + c)
+
+top_5_movies("pacific", "rim", "iron", "man", "7")
+
+def add(x, y):
+    print(x + y)
+
+add(5, 10)
+add(55, 79)
+add("cat", "dog")
