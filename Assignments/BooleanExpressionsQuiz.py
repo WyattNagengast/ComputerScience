7a83bd4ebd0f16da2560d77301a49b2b3711f068
WyattNagengast <820635@mystma.org>
2024-10-04 14:48:04 -0500

    10/4

---
+++ Assignments/BooleanExpressionsQuiz.py
@@ -0,0 +1,10 @@
+x = int(input("Give a whole number greater than -5.\n> "))      #askes for x > -5
+print(x > -5)                                                   #verifys truth
+z = int(input("Give a whole number eaqual to 1 + 0.\n> "))
+print(z = 1)
+y = int(input("Give a whole number less than 100 but greater than " + z + ".\n> "))
+print(100 > y > z)
+a = int(input("Give a number less than " + y + ".\n> "))
+print(a < y)
+b = int(input("Give a whole number that is greater than or equal to" + a + "but less than " + y + ".\n> "))
+print(a <= b < y)
