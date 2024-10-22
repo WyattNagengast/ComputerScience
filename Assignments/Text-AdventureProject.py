98356e13a0dad8768a7fbec397c02e1d1dbcdac6
WyattNagengast <820635@mystma.org>
2024-10-22 11:26:42 -0500

    snapoct22

--- Assignments/Text-AdventureProject.py
+++ Assignments/Text-AdventureProject.py
@@ -18,39 +18,91 @@ def start_adventure():
     print("You grab the " + wc + " and head for the forest.")
     reach_forest()
 
-def reach_forest():
-    print("Once you reach the forest you begin to reconsider going hunting today, because it is very dark out now. Do you:")
-    print("1. enter the forest to hunt")
-    print("2. head home")
+    def reach_forest():
+        print("Once you reach the forest you begin to reconsider going hunting today, because it is very dark out now. Do you:")
+        print("1. enter the forest to hunt")
+        print("2. head home")
 
-    choice = input("> ")
-    if choice == "1":
-        enter_forest()
-    elif choice == "2":
-        head_home()         #ending 1
-    else:
-        print("Invalid answer please try again.")
-        reach_forest()
+        choice = input("> ")
+        if choice == "1":
+            enter_forest()
+        elif choice == "2":
+            head_home()         #endding 1
+        else:
+            print("Invalid answer please try again.")
+            reach_forest()
 
-def head_home():
-    print("You decide that its better to head home, but your adventure ends here.")
+    def head_home():        #endding 1
+        print("You decide that its better to head home, but your adventure ends here.")
 
-def enter_forest():
-    print("You bravely head into the forest to hunt.")
-    print("After a while of walking you hear an animal nearby, but you are not sure what animal you hear. Do you:")
-    print("1. check out what animal it is")
-    print("2. continue on and look for a different animal")
+    def enter_forest():
+        print("You bravely head into the forest to hunt.")
+        print("After a while of walking you hear an animal nearby, but you are not sure what animal you hear. Do you:")
+        print("1. check out what animal it is")
+        print("2. continue on and look for a different animal")
 
-    choice = input("> ")
-    if choice == "1":
-        find_animal1()
-    elif choice == "2":
-        continue_search()
-    else:
-        print("Invalid answer please try again.")
-        enter_forest()
+        choice = input("> ")
+        if choice == "1":
+            find_animal1()
+        elif choice == "2":
+            continue_search()
+        else:
+            print("Invalid answer please try again.")
+            enter_forest()
 
-def find_animal1():
-    print("You find the animal and it is a black bear. Do you")
-    print("1. prepare to atack")
-    print("2. sneak away and flea")
+    def find_animal1():
+        print("You find the animal and it is a black bear. Do you")
+        print("1. prepare to atack")
+        print("2. sneak away and flea")
+
+        choice = input("> ")
+        if choice == "1":
+            attack_prep()
+        elif choice == "2":
+            flea()
+        else:
+            print("Invalid answer please try again.")
+            find_animal1()
+
+    def flea():
+        print("After you flea you start to consider just heading back for the night. Do you:")
+        print("1. head home")
+        print("2. countinue search")
+
+        choice = input("> ")
+        if choice == "1":
+            head_home()     #endding 1
+        elif choice == "2":
+            continue_search()
+        else:
+            print("Invalid answer please try again.")
+            flea()
+
+    def continue_search():
+        print("You continue searching for prey, but after awhile you start to debate giving up in defeat. Do you:")
+        print("1. countinue searching")
+        print("2. give up and head home without food")
+
+        choice = input("> ")
+        if choice == "1":
+            search_continues()
+        elif choice == "2":
+            attempt_to_head_home()      #endding 2
+        else:
+            print("Invalid answer please try again.")
+            continue_search()
+    
+    def search_continues():
+
+    def attempt_to_head_home():         #endding 2
+        print("You begin to head home.\nBut you collapse out of hunger half way home.")
+        print("You do not make it home.")
+
+    def attack_prep():
+        print("You decide that you are going to attempt to hunt the Black Bear. Do you:")
+        if wc == "hatchet":
+            hatchet_attacks()
+        elif wc == "crossbow":
+            crossbow_attacks()
+        elif wc == "dagger":
+            dagger_attacks()
